3e70d9f352c594a9b11160add97045cf1c78c718
michalshy <91937056+michalshy@users.noreply.github.com>
2024-11-13 19:10:06 +0100

    cleanup

--- Src/main.c
+++ Src/main.c
@@ -31,28 +31,25 @@
 #define PINNO(pin) (pin & 255)
 #define PINBANK(pin) (pin >> 8)
 
-struct gpio {
-  volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
-};
-#define GPIO(bank) ((struct gpio *) (D3_AHB1PERIPH_BASE + 0x400 * (bank)))
+#define GPIO(bank) ((GPIO_TypeDef *) (D3_AHB1PERIPH_BASE + 0x400 * (bank)))
 
 // Enum values are per datasheet: 0, 1, 2, 3
 enum { GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG };
 
 static inline void gpio_set_mode(uint16_t pin, uint8_t mode) {
-  struct gpio *gpio = GPIO(PINBANK(pin));  // GPIO bank
-  int n = PINNO(pin);                      // Pin number
-  gpio->MODER &= ~(3U << (n * 2));         // Clear existing setting
-  gpio->MODER |= (mode & 3) << (n * 2);    // Set new mode
+	GPIO_TypeDef * gpio = GPIO(PINBANK(pin));  // GPIO bank
+	int n = PINNO(pin);                      // Pin number
+	gpio->MODER &= ~(3U << (n * 2));         // Clear existing setting
+	gpio->MODER |= (mode & 3) << (n * 2);    // Set new mode
 }
 
 static inline void gpio_write(uint16_t pin, bool val) {
-  struct gpio *gpio = GPIO(PINBANK(pin));
-  gpio->BSRR = (1U << PINNO(pin)) << (val ? 0 : 16);
+	GPIO_TypeDef * gpio = GPIO(PINBANK(pin));
+	gpio->BSRR = (1U << PINNO(pin)) << (val ? 0 : 16);
 }
 
 static inline bool gpio_read(uint16_t pin) {
-	struct gpio *gpio = GPIO(PINBANK(pin));
+	GPIO_TypeDef * gpio = GPIO(PINBANK(pin));
 	return gpio->IDR;
 }
 
